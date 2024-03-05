00fc13b4767deec1df0f6579c9c0a9bf9c450fe2
Fernando <ferelbue@gmail.com>
2024-03-05 12:11:59 +0100

    feat:conexion to mongo DB

---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+
+import mongoose from "mongoose";
+
+
+export const dbConnection = () => {
+    return mongoose.connect(
+        process.env.MONGO_URI,
+        {}
+        );
+
+
+
+}
+
+
--- src/server.js
+++ src/server.js
@@ -2,15 +2,27 @@
 // IMPORTAMOS DEPENDENCIAS DE LIBRERIAS
 import express from 'express';
 import "dotenv/config";
+import { dbConnection } from './database/db.js';
 
-const app =express();
+const app = express();
 
 //PARSEA EL BODY
 app.use(express.json())
 
 const PORT = process.env.PORT || 4001;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+dbConnection()
+    .then(() => {
+
+        console.log('Database connected')
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+    })
+    .catch(error => {
+
+        console.log(error)
+    })
+
 
