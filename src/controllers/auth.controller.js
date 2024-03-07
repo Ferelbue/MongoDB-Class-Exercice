68ac2e6e44a64c5b74505913a0d0e0904ffc616c
Fernando <ferelbue@gmail.com>
2024-03-07 12:31:31 +0100

    feat:create user login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 
@@ -21,7 +22,7 @@ export const register = async (req, res) => {
 
         //Email validation
         const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-        
+
         if (!validEmail.test(email)) {
             return res.status(400).json({
                 success: false,
@@ -60,11 +61,77 @@ export const register = async (req, res) => {
 
 
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
 
+    try {
 
-    res.status(200).json({
-        success: true,
-        message: "Register user succesfully",
-    })
+        //Retrieve data
+        const email = req.body.email
+        const password = req.body.password
+
+        //Validation data
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "email and password are mandatories"
+            })
+        }
+
+        const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        if (!validEmail.test(email)) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: "Email format is not valid"
+                }
+            )
+        }
+
+        //Find in BD
+        const user = await User.findOne(
+            {
+                email: email
+            }
+            )
+            
+            if (!user) {
+                res.status(400).json({
+                    success: false,
+                    message: "Email or password invalid"
+                })
+            }
+            
+        const isValidPassword = bcrypt.compareSync(password, user.password)
+
+        if (!isValidPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "Email or password invalid"
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                roleName: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "2h"
+            }
+        )
+        res.status(200).json({
+            success: true,
+            message: "User logged succesfully",
+            token: token
+        })
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: "User cant be logged",
+            error: error
+        })
+    }
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,8 @@ import { login, register } from "../controllers/auth.controller.js";
 
 const router = Router()
 
-router.post('/register', register)
-router.post('/login', login)
+// router.post('/register', register)
+// router.post('/login', login)
 
 export default router
 
