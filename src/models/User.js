bd92a828771730cf35cfa60e63ede68c317c08d4
Fernando <ferelbue@gmail.com>
2024-03-07 10:30:25 +0100

    feat:inicio clase

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+
+import { Schema, model } from "mongoose";
+
+export const UserSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: false
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role:{
+            type: String,
+            enum:['user','admin','super-admin'],
+            default:'user'
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+const User = model('User',UserSchema)
+
+export default User
